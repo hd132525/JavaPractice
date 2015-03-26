7aad8e81edbf619aa5f60e240cb01d2b69587a10
tamigun <tamigun.root@gmail.com>
2015-03-26 11:33:45 +0900

    포문, 수학식 사용 (포문 주석처리) : 2015.3.26

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -1,9 +1,13 @@
 package org.dimigo.basic;
 
+//import java.util.Scanner;
+
 public class Condition {
 	public static void main(String[] args){
-		int length=10, cost, add;
+		int length=30, cost, add;
 		String value = "고속버스";
+		
+		
 		
 		if(value.equals("고속버스")) {
 			cost = 850; add = 300;
@@ -13,13 +17,17 @@ public class Condition {
 			cost = 600; add = 200;
 		}
 		
-		for(int i=20; i<=length; i+=10) {
+		
+		cost += add * ((length-10) / 10);
+		//Using FOR
+		/*for(int i=20; i<=length; i+=10) {
 			cost += add;
-		}
+		}*/
 		
 		System.out.println("<< 고속도로 통행료 계산 >>");
 		System.out.println("거리: " + length + "km");
 		System.out.println("차량: " + value);
 		System.out.println("통행료: " + cost + "원");
+		
 	}
 }
