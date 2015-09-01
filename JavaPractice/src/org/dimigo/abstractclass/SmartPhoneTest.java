f8baf003e84b7676d3e121eb4fbbac57dd112523
배주웅 <tamigun.root@gmail.com>
2015-09-01 10:16:38 +0900

    Create abstractclass package  ㄴChange SmartPhone to Abstract Class

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,24 @@
+package org.dimigo.abstractclass;
+
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		IPhone iphone = new IPhone("iPhone 6", "애플", 700000);
+		Galaxy galaxy = new Galaxy("갤럭시 S6", "삼성", 650000);
+		
+		System.out.println(iphone.toString());
+		iphone.turnOn();
+		iphone.pay();
+		iphone.useSpecialFunction();
+		iphone.turnOff();
+		
+		System.out.println();
+		
+		System.out.println(galaxy.toString());
+		galaxy.turnOn();
+		galaxy.pay();
+		galaxy.useSpecialFunction();
+		galaxy.turnOff();
+	}
+
+}
